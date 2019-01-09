7ed69e9676b3d1cc70996f6280997a114cbecccf
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-09 13:05:23 -0500

    Main.js being checked in, but appears to be using deprecated code

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const Apify = require('apify');
 
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: 'https://www.iana.org/' });
-    const pseudoUrls = [new Apify.PseudoUrl('https://www.iana.org/[.*]')];
+    await requestQueue.addRequest({ url: 'https://www.visitrichmondva.com/event/rva-on-ice/17800/' });
+    const pseudoUrls = [new Apify.PseudoUrl('https://www.visitrichmondva.com/event/rva-on-ice/17800/')];
 
     const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
@@ -12,8 +12,8 @@ Apify.main(async () => {
             console.log(`Title of ${request.url}: ${title}`);
             await Apify.utils.puppeteer.enqueueLinks(page, 'a', pseudoUrls, requestQueue);
         },
-        maxRequestsPerCrawl: 100,
-        maxConcurrency: 10,
+        maxRequestsPerCrawl: 5,
+        maxConcurrency: 5,
     });
 
     await crawler.run();
