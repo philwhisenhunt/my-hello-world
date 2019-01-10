29e4e8ab0b21a0219e37c51bf6e1f4af6deb91ce
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-10 10:47:16 -0500

    Now pulling the data correctly. Pagination is next

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const Apify = require('apify');
 Apify.main(async () => {
     // Get the queue and start with the first one
     const requestQueue = await Apify.openRequestQueue();
+    //Puts RVA link in the queue
     await requestQueue.addRequest(new Apify.Request({url: 'https://www.visitrichmondva.com/event/rva-on-ice/17800/'}));
 
+    //makes a new PuppeteerCrawler Object
     const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
 
@@ -21,10 +23,29 @@ Apify.main(async () => {
                 price: null,
                 website: null,
             }
+            //
             event.url = request.url;
-        }
-    })
+            event.name = await page.$eval('div.detailInfo div.inner h1', h1 => h1.innerText);
+            event.address = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="address"]', li =>li.innerText);
+            event.dates = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="dates"]', li =>li.innerText);
+            event.reccurence = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="recur"]', li =>li.innerText);
 
 
-}
+            //target the li with the data-name:address
+            
+            //add more selectors here
+            console.log(event);
+        },
+
+        //If fail 4 times, then execute this:
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed 4 times`);
+        },
+    });
+
+    //Run crawler.
+    await crawler.run();
+
+
+});
 
