79de214052b6592bfc2f681d65f065dfb5c70c07
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-12 15:51:18 -0500

    checking in working log

--- workingLogMain.js
+++ workingLogMain.js
@@ -1,28 +1,51 @@
 const Apify = require('apify');
 
 Apify.main(async () => {
+    // Get the queue and start with the first one
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: 'https://www.visitrichmondva.com/event/rva-on-ice/17800/' });
-    
-    const handlePageFunction = async ({ request, $ }) => {
-        const headline = $('.inner h1').text();
-    
-        console.log(`The headline of "${request.url}" is: ${headline}.`);
+    //Puts RVA link in the queue
+    await requestQueue.addRequest(new Apify.Request({url: 'https://www.visitrichmondva.com/event/'}));
 
-        const info = $('.info-list.address').text();
-
-        console.log(`The info is ${info}`);
-
-        // const dates = $('.info-list-label:eq(0)').text();
-
-        // console.log(`The info is ${dates}`);
-    }
-    
-    // Set up the crawler, passing a single options object as an argument.
-    const crawler = new Apify.CheerioCrawler({
+    //makes a new PuppeteerCrawler Object
+    const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
-        handlePageFunction
+
+        handlePageFunction: async ({ page, request }) => {
+            let event = {
+                url: null,
+                name: null,
+                address: null,
+                dates: null,
+                reccurence: null,
+                venue: null,
+                phone: null,
+                time: null,
+                price: null,
+                website: null,
+            }
+            //
+            event.url = request.url;
+            event.name = await page.$eval('div.detailInfo div.inner h1', h1 => h1.innerText);
+            event.address = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="address"]', li =>li.innerText);
+            event.dates = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="dates"]', li =>li.innerText);
+            event.reccurence = await page.$eval('div.detailInfo div.inner ul.info-list li[data-name="recur"]', li =>li.innerText);
+
+
+            //target the li with the data-name:address
+            
+            //add more selectors here
+            console.log(event);
+        },
+
+        //If fail 4 times, then execute this:
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed 4 times`);
+        },
     });
-    
+
+    //Run crawler.
     await crawler.run();
+
+
 });
+
